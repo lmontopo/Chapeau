01b351e2c584118c9454332b20174c58512dc06e
Leta Montopoli <lmontopo@gmail.com>
2014-12-01 18:15:11 -0500

    fixed install module, bumped version

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
 	name = 'Chapeau',
-	version = '0.0.1',
+	version = '0.0.2',
 	url = 'https://github.com/lmontopo/Chapeau',
 	author = 'Leta Montopoli and Margo Smith',
 	description='A very lightweight web framework',
 	author_email = 'lmontopo@gmail.com',
-	modules = ['chapeau.py'] )
+	py_modules = ['chapeau'] )
